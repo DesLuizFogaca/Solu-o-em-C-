f6b5454bc8e7859b1f12d351f330dbe24ce01c44
Luiz Fogaca <fogacaluiz13@hotmail.com>
2022-11-07 14:23:29 -0300

    Solução de exercicío em C#

---
+++ Course/Entities/HourContract.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Course.Entities
+{
+    class HourContract
+    {
+        public DateTime Date { set; get; }
+        public double ValuePerHour { get; set; }
+        public int Hours { get; set; }
+        public HourContract()
+        {
+        }
+        public HourContract(DateTime date, double valuePerHour, int hours)
+        {
+            Date = date;
+            ValuePerHour = valuePerHour;
+            Hours = hours;
+        }
+        public double TotalValue()
+        {
+            return Hours * ValuePerHour;
+        }
+    }
+}
